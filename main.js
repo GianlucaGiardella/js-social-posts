ab452f6e4a02a5ebf415597e463f6876147d6e0a
GianluGiarde <gianlugiarde@gmail.com>
2023-05-03 15:53:00 +0200

    Add foreach post

--- main.js
+++ main.js
@@ -55,3 +55,40 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.querySelector("#container");
+
+posts.forEach(profile => {
+    container.innerHTML += `        
+<div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${profile.author.image}" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${profile.author.name}</div>
+                <div class="post-meta__time">${profile.created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${profile.content}</div>
+    <div class="post__image">
+        <img src="${profile.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${profile.id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${profile.likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+</div>
+`;
+})
